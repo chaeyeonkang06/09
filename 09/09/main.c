3dbb58dc6cf6205a7fe74700286f71549889d855
chaeyeonkang06 <hellokcy@sookmyung.ac.kr>
2025-10-30 16:04:52 +0900

    3

--- 09/09/main.c
+++ 09/09/main.c
@@ -11,19 +11,38 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    int i, average;
+#if 0
+    int i;
     int grade[SIZE];
-    int sum;
-
-    sum = 0;
+    int score[SIZE];
+    
+    for(i=0;i<SIZE;i++)
+        grade[i] = rand()%100;
+    
+    for(i=0;i<SIZE;i++)
+    score[i]=grade[i];
+    
+    for(i=0;i<SIZE;i++)
+        printf("grade[%d]=%d, score[%d]=%d\n",
+               i, grade[i], i, score[i]);
+    
+    return 0;
+#endif
+    
+    int i;
+    int a[SIZE] = {1, 2, 3, 4, 5};
+    int b[SIZE] = {1, 2, 3, 4, 5};
+    int flag_same=1;
+    
     for(i=0;i<SIZE;i++)
     {
-        printf("학생 성적을 입력하세요.: ");
-        scanf("%d", &grade[i]);
-        sum += grade[i];
+        if(a[i] != b[i])
+        {
+            printf("%i번째 요소가 다릅니다.\n", i);
+            flag_same = 0;
+        }
     }
-    average = sum/SIZE;
-    printf("성적 평균 : %d\n", average);
+    printf("a와 b가 같습니까? %i\n", flag_same);
     
     return 0;
 }
