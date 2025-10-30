6a7879895c21b880b0a0408dd0be4c59f3fe0ecc
chaeyeonkang06 <hellokcy@sookmyung.ac.kr>
2025-10-30 16:13:54 +0900

    4

--- 09/09/main.c
+++ 09/09/main.c
@@ -6,43 +6,35 @@
 //
 
 #include <stdio.h>
-#include <stdlib.h>
-#define SIZE 5
+#define SIZE 4
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-#if 0
-    int i;
-    int grade[SIZE];
-    int score[SIZE];
+void square_array(int a[], int size);
+void print_array(int a[], int size);
+
+int main(void)
+{
+    int list[SIZE]={1, 2, 3, 4};
     
-    for(i=0;i<SIZE;i++)
-        grade[i] = rand()%100;
-    
-    for(i=0;i<SIZE;i++)
-    score[i]=grade[i];
-    
-    for(i=0;i<SIZE;i++)
-        printf("grade[%d]=%d, score[%d]=%d\n",
-               i, grade[i], i, score[i]);
+    print_array(list, SIZE);
+    square_array(list, SIZE);
+    print_array(list, SIZE);
     
     return 0;
-#endif
-    
+}
+
+void square_array(int a[], int size)
+{
     int i;
-    int a[SIZE] = {1, 2, 3, 4, 5};
-    int b[SIZE] = {1, 2, 3, 4, 5};
-    int flag_same=1;
     
-    for(i=0;i<SIZE;i++)
-    {
-        if(a[i] != b[i])
-        {
-            printf("%i번째 요소가 다릅니다.\n", i);
-            flag_same = 0;
-        }
-    }
-    printf("a와 b가 같습니까? %i\n", flag_same);
+    for(i=0;i<size; i++)
+        a[i] = a[i]*a[i];
+}
+
+void print_array(int a[], int size)
+{
+    int i;
     
-    return 0;
+    for(i=0;i<size;i++)
+        printf("%3d", a[i]);
+    printf("\n");
 }
